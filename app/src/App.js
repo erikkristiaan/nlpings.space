b050a243fa2788b93d6f9f52d4a486fb033c33f1
Erik <allardae@gmail.com>
2022-06-12 10:29:33 +0200

    Added infinite scroll & spinner

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import './components/spinner/spinner.styles.css';
+
 import getIconTable from './iconTable';
 
 import InfiniteScroll from 'react-infinite-scroll-component';
 import React from 'react';
 
+import { TitleBar } from './components/titlebar/titlebar.component';
+import { NavBar } from './components/navbar/navbar.component';
 import { PostCard } from './components/postcard/postcard.component';
 import { Container } from 'react-bootstrap';
 
@@ -15,34 +19,27 @@ class App extends React.Component {
     this.state = {
       pings: [],
       isFetched: false,
+      pageNum: 0,
     }
   }
 
-  // getIconTable = () => {
-  //   const iconTable = {
-
-  //     "METAL": "fa-music",
-  //     "FURRY": "fa-paw",
-  //     "DOG": "fa-dog",
-  //     "MOVIES": "fa-clapperboard",
-  //     "TRAVEL": "fa-map-location-dot",
-  //     "ALPHABET-MAFIA": "fa-rainbow",
-  //     "GAMING": "fa-gamepad",
-  //     "FITNESS": "fa-person-running",
-  //     "DYEL": "fa-dumbbell",
-  //     "PHOTO": "fa-camera",
-  //     "MAMADAS": "fa-earth-americas",
-  //     "CAN": "fa-earth-americas",
-  //     "USA-OR": "fa-flag-usa",
-  //   };
-  //   return iconTable;
-  // }
-
   componentDidMount() {
-    fetch('http://localhost:3001/api/pings/main/1')
+    fetch(`http://localhost:3001/api/pings/main/${this.state.pageNum}`)
       .then(response => response.json())
       .then(data => {
-        this.setState( () => { return { pings: data, isFetched: true}})
+        this.setState( () => { return { pings: data, isFetched: true, pageNum: this.state.pageNum + 1}})
+      });
+  }
+
+  fetchMoreData = () => {
+    console.log('tried to fetch')
+    fetch(`http://localhost:3001/api/pings/main/${this.state.pageNum}`)
+      .then(response => response.json())
+      .then(data => {
+        let oldArr = this.state.pings;
+        setTimeout( () => {
+          this.setState( () => { return { pings: oldArr.concat(data), isFetched: true, pageNum: this.state.pageNum + 1}})
+        }, 0); // <- Add 1200 to simulate page loads
       });
   }
 
@@ -51,13 +48,25 @@ class App extends React.Component {
     const iconTable = getIconTable();
     return (
       <div className="App">
+        <TitleBar />
+        <NavBar />
         <Container>
-          <h1 className="title">r/Neoliberal Pings</h1>
-          <div>
-            { isFetched 
-                 ? pings.map((ping, index) => <PostCard key={index} ping={ping} icon={iconTable}/>) 
-                 : 'Loading...' }
-          </div>
+          {/* <h1 className="title">r/Neoliberal Pings</h1> */}
+
+          <InfiniteScroll
+            dataLength={pings.length}
+            hasMore={true}
+            next={this.fetchMoreData}
+            loader={<span className="loader" />}
+            scrollThreshold={0.9}
+            style={{overflow: 'visible'}}
+          >
+            <div>
+              { isFetched 
+                  ? pings.map((ping, index) => <PostCard key={index} ping={ping} icon={iconTable}/>) 
+                  : <span className="loader" /> }
+            </div>
+          </InfiniteScroll>
         </Container>
       </div>
     );
