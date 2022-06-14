bb81dcabb1655bd579a92f31feac0c78086367a6
Erik <allardae@gmail.com>
2022-06-14 15:22:46 +0200

    minor cleanup, added keys, css styling

--- app/src/components/navbar/navbar.component.jsx
+++ app/src/components/navbar/navbar.component.jsx
@@ -1,5 +1,5 @@
 import './navbar.styles.css';
-import getItems from './navbar-items';
+import { navbarItems } from './navbar-items';
 
 import React from 'react';
 import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap';
@@ -7,16 +7,11 @@ import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap';
 class NavBar extends React.Component {
 
   render() {
-
-    const navItems = getItems();
-
     return(
-      <div>
-        <Navbar bg='light' expand='lg'>
-          <Container>
-            {/* <Navbar.Brand>
-              <h1>r/neoliberal pings</h1>
-            </Navbar.Brand> */}
+      <div className="navbar-div">
+        <Navbar bg='dark' variant='dark' expand='lg' fixed='top'>
+          <Container fluid>
+            <Navbar.Brand>r/neoliberal pings</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className='me-auto'>
@@ -24,36 +19,41 @@ class NavBar extends React.Component {
                 <NavDropdown 
                   title='Special' 
                   id='basic-nav-dropdown'
+                  menuVariant='dark'
                 >
-                  { navItems.special.map( (i) => <NavDropdown.Item>{i}</NavDropdown.Item> ) }
+                  { navbarItems.special.map( (i) => <NavDropdown.Item key={i}>{i}</NavDropdown.Item> ) }
                 </NavDropdown>             
 
                 <NavDropdown 
                   title='International' 
                   id='basic-nav-dropdown'
+                  menuVariant='dark'
                 >
-                  { navItems.itl.map( (i) => <NavDropdown.Item>{i}</NavDropdown.Item> ) }
+                  { navbarItems.itl.map( (i) => <NavDropdown.Item key={i}>{i}</NavDropdown.Item> ) }
                 </NavDropdown>
 
                 <NavDropdown 
                   title='United States' 
                   id='basic-nav-dropdown'
+                  menuVariant='dark'
                 >
-                  { navItems.usa.map( (i) => <NavDropdown.Item>{i}</NavDropdown.Item> ) }
+                  { navbarItems.usa.map( (i) => <NavDropdown.Item key={i}>{i}</NavDropdown.Item> ) }
                 </NavDropdown>
 
                 <NavDropdown 
                   title='Interests & Fun' 
                   id='basic-nav-dropdown'
+                  menuVariant='dark'
                 >
-                  { navItems.fun.map( (i) => <NavDropdown.Item>{i}</NavDropdown.Item> ) }             
+                  { navbarItems.fun.map( (i) => <NavDropdown.Item key={i}>{i}</NavDropdown.Item> ) }             
                 </NavDropdown>
 
                 <NavDropdown
                   title='Academia & Work' 
                   id='basic-nav-dropdown'
+                  menuVariant='dark'
                 >
-                  { navItems.academia.map( (i) => <NavDropdown.Item>{i}</NavDropdown.Item> ) }             
+                  { navbarItems.academia.map( (i) => <NavDropdown.Item key={i}>{i}</NavDropdown.Item> ) }             
                 </NavDropdown>
 
               </Nav>
@@ -65,4 +65,4 @@ class NavBar extends React.Component {
   }
 }
 
-export {NavBar};
+export { NavBar };
