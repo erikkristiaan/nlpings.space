1a9415197747592a85e7bb21b93572b3a37fd718
Erik Allard <allardae@gmail.com>
2022-06-25 06:18:00 +0000

    changed root path to build folder

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import { connectToDatabase, getDb } from './db/conn.js';
 import pings from './routes/pings.js'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 connectToDatabase();
 
@@ -11,6 +17,15 @@ const app = express();
 // const router = express.Router();
 app.use(cors());
 app.use('/api/pings', pings);
+app.use(express.static(path.join(__dirname, '../app/build')));
+
+// app.use((req, res, next) => {
+//     res.redirect('/');
+// });
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../app/build', 'index.html'))
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is listening on port ${process.env.PORT}`);
