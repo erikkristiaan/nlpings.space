fb9ace83d3897775c64f1a682ef2a1d8589840b9
Erik <allardae@gmail.com>
2022-07-08 06:09:32 +0200

    replace luxon with dayjs and convert to hook

--- app/src/components/postcard/postcard.component.jsx
+++ app/src/components/postcard/postcard.component.jsx
@@ -1,48 +1,72 @@
-import './postcard.styles.css'
-
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { DateTime } from 'luxon';
+import dayjs from 'dayjs';
 import parse from 'html-react-parser';
 import { Card, Button } from 'react-bootstrap';
 
-class PostCard extends React.Component {
+import './postcard.styles.css';
 
-  render() {
-    const p = this.props.ping;
-    const icon = `fa-solid ${this.props.icon[p.ping_group]}`;
-    const dt = DateTime.fromSeconds(p.time);
-    const localeDT = dt.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+export default function PostCard({ icon, ping }) {
 
-    return (
-      <div>
-        <Card className="post-card">
-          <Card.Header>
-            <div className="post-card-header">
-              <h5><i className={icon}></i>&nbsp;&nbsp;{p.ping_group}</h5>
-              <h6 className="date-posted">{localeDT}</h6>
-            </div>
-          </Card.Header>
-          <Card.Body>
-            <div>
-              <Card className="post-card-subheader">
-                <Card.Body className="post-card-subheader-body">
-                  <p className="subtext">posted by <a href={`https://www.reddit.com/u/${p.author}`}>u/{p.author}</a> in {p.parent_submission}</p>
-                </Card.Body>
-              </Card>
-            </div>
-            <div className="html-body">{parse(p.body.html)}</div>
-          </Card.Body>
-          <Card.Footer>
-            <Button variant="primary" href={`https://www.reddit.com${p.permalink}?context=10000`}><i className="fa-solid fa-link" /> Permalink</Button>
-            <Button variant="primary" href={`https://www.reddit.com${p.permalink}`.slice(0,-8)}><i className="fa-solid fa-arrow-up-short-wide" /> Parent Submission</Button>
-            <Button variant="primary" as={Link} to={`/search?a=${p.author}`}><i className="fa-solid fa-user" /> User Pings</Button>
-          </Card.Footer>
-        </Card>
-      </div>
-    );
-  };
+  const { 
+    author,
+    body,
+    parent_submission,
+    permalink,
+    ping_group,
+    time,
+  } = ping;
+
+  const unixTime = dayjs.unix(time);
+  const formattedTime = dayjs(unixTime).format('MMM D, YYYY, h:mm A');
+  const iconString = `fa-solid ${icon[ping_group]}`;
+
+  return (
+    <>
+      <Card className='post-card'>
+        <Card.Header>
+          <div className='post-card-header'>
+            <h5>
+              <i className={iconString}></i>&nbsp;&nbsp;{ping_group}
+            </h5>
+            <h6 className='date-posted'>{formattedTime}</h6>
+          </div>
+        </Card.Header>
+        <Card.Body>
+            <Card className='post-card-subheader'>
+              <Card.Body className='post-card-subheader-body'>
+                <p className='subtext'>
+                  posted by{' '}
+                  <a href={`https://www.reddit.com/u/${author}`}>u/{author}</a>{' '}
+                  in {parent_submission}
+                </p>
+              </Card.Body>
+            </Card>
+          <div className='html-body'>{parse(body.html)}</div>
+        </Card.Body>
+        <Card.Footer>
+          <Button
+            variant='primary'
+            href={`https://www.reddit.com${permalink}?context=10000`}
+          >
+            <i className='fa-solid fa-link' /> Permalink
+          </Button>
+          <Button
+            variant='primary'
+            href={`https://www.reddit.com${permalink}`.slice(0, -8)}
+          >
+            <i className='fa-solid fa-arrow-up-short-wide' /> Parent Submission
+          </Button>
+          <Button 
+            variant='primary' 
+            as={Link} 
+            to={`/search?a=${author}`}
+          >
+            <i className='fa-solid fa-user' /> User Pings
+          </Button>
+        </Card.Footer>
+      </Card>
+    </>
+  );
 }
-  
-export default PostCard;
