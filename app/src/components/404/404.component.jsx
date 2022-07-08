352a12a9eb8709c2a023fd4833e406b282decc88
Erik <allardae@gmail.com>
2022-07-08 06:10:26 +0200

    convert to hooks

--- app/src/components/404/404.component.jsx
+++ app/src/components/404/404.component.jsx
@@ -1,16 +1,11 @@
-import './404.styles.css'
+import React from "react";
+import "./404.styles.css";
 
-import React from 'react';
-
-class FourOhFour extends React.Component {
-  render(){
-    return(
-      <div className='four-oh-four'>
-        <h2>404: Page Not Found</h2>
-        <h5>The page you requested doesn't seem to exist. 😕</h5>
-      </div>
-    )
-  }
+export default function FourOhFour() {
+  return (
+    <div className='four-oh-four'>
+      <h2>404: Page Not Found</h2>
+      <h5>The page you requested doesn't seem to exist. 😕</h5>
+    </div>
+  );
 }
-
-export default FourOhFour;
