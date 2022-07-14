19ea10ae0b11b92a5722ba0cc8ab6da7d3527011
Erik <allardae@gmail.com>
2022-07-14 12:39:51 +0200

    move cors

--- server/server.js
+++ server/server.js
@@ -11,11 +11,11 @@ import pings from './routes/pings.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const app = express();
+app.use(cors()); // enable cors when react app is being run in an seperate development enviroment
+
 connectToDatabase();
 
-const app = express();
-
-// app.use(cors()); // enable cors when react app is being run in an seperate development enviroment
 app.use('/api/pings', pings);
 app.use(express.static(path.join(__dirname, '../app/build')));
 
